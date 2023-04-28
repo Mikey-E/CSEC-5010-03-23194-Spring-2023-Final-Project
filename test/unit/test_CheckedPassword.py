078f22f79736d1363b78ea305652d04d98c165d1
Michael <michaelelgin1@gmail.com>
2023-04-28 17:05:19 -0600

    fixed improper assertion for CheckedPassword

--- test/unit/test_CheckedPassword.py
+++ test/unit/test_CheckedPassword.py
@@ -19,28 +19,28 @@ def test_policy_criteria():
 	assert "Suggested numbers: 1" in cpw.warnings, "no warning for having no numbers in password"
 	assert "Suggested uppercase characters: 1" in cpw.warnings, "no warning for having no uppercase characters"
 	assert "Suggested special characters: 1" in cpw.warnings, "no warning for having no special characters"
-	assert "One of the top 100k passwords" not in cpw.warnings
+	assert "One of the most common passwords" not in cpw.warnings
 
 def test_numbers():
 	cpw = CheckedPassword("asdfasdfasdfA!")
 	expectedWarningsAmt = 1
 	assert len(cpw.warnings) == expectedWarningsAmt, "length of conditions was not " + str(expectedWarningsAmt)
 	assert "Suggested numbers: 1" in cpw.warnings, "no warning for having no numbers in password"
-	assert "One of the top 100k passwords" not in cpw.warnings
+	assert "One of the most common passwords" not in cpw.warnings
 
 def test_uppercase():
 	cpw = CheckedPassword("asdfasdfasdf1!")
 	expectedWarningsAmt = 1
 	assert len(cpw.warnings) == expectedWarningsAmt, "length of conditions was not " + str(expectedWarningsAmt)
 	assert "Suggested uppercase characters: 1" in cpw.warnings, "no warning for having no uppercase characters"
-	assert "One of the top 100k passwords" not in cpw.warnings
+	assert "One of the most common passwords" not in cpw.warnings
 
 def test_specials():
 	cpw = CheckedPassword("asdfasdfasdfA1")
 	expectedWarningsAmt = 1
 	assert len(cpw.warnings) == expectedWarningsAmt, "length of conditions was not " + str(expectedWarningsAmt)
 	assert "Suggested special characters: 1" in cpw.warnings, "no warning for having no special characters"
-	assert "One of the top 100k passwords" not in cpw.warnings
+	assert "One of the most common passwords" not in cpw.warnings
 
 def test_all_errors():
 	cpw = CheckedPassword("sunshine")
